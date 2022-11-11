9dc9d3588fe46621695b01072df8850cd37572bf
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-11-11 20:14:01 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BitcoinserviceService } from './bitcoinservice.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ],
+  providers: [
+    BitcoinserviceService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
